af554cb5ebd0be1e6e23fa809f5a9d8bfbacccb8
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-06-20 23:36:12 +0530

    Added JS for color changing

--- background-toggler/src/App.js
+++ background-toggler/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 import Header from './components/Header/header';
+import Form from './components/Form/form';
+import { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [bgColor, setBgColor] = useState("");
+
+  const changeInputValue = e => {
+    setInputValue(e.target.value);
+  }
+  
+  const changeBgColor = (e) => {
+    e.preventDefault();
+    setBgColor(inputValue);
+  }
+
   return (
-    <div className="App">
+    <div style={{ backgroundColor: `${bgColor}` }} className="App">
       <Header />
+      <Form onSubmission={changeBgColor} inputValue={inputValue} changeInputValue = {changeInputValue}/>
     </div>
   );
 }
