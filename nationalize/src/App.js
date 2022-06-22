99282f78c9ad23242125311e6451aed107a03bdd
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-06-22 23:46:00 +0530

    Implemented 'Countries' component

--- nationalize/src/App.js
+++ nationalize/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from "./components/Header/header";
 import Form from "./components/Form/form";
+import Countries from './components/Countries/countries';
 import { useState } from "react";
 
 function App() {
   const [inputValue, setInputValue] = useState("");
   const [countries, setCountries] = useState([]);
+  const [submitted, setSubmitted] = useState();
 
   const changeInputValue = e => {
     setInputValue(e.target.value);
@@ -18,13 +20,14 @@ function App() {
         await fetch(`https://api.nationalize.io/?name=${inputValue}`)
       ).json()
     );
-    console.log(countries);
+    setSubmitted(true);
   }
 
   return (
     <div className="App">
       <Header/>
       <Form onSubmission={fetchData} inputValue={inputValue} changeInputValue = {changeInputValue}/>
+      {submitted && <Countries data={countries}/>}
     </div>
   );
 }
